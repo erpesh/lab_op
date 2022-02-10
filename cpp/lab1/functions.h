2e53ddc4f6532329914d5e3a158d9be5e01261fd
erpesh <89254828+erpesh@users.noreply.github.com>
2022-02-10 12:29:03 +0200

    Add files via upload

---
+++ cpp/lab1/functions.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void inputFile(string);
+void outputFile(string);
+void editFile(string, string);
