c2f4994ece5b8c8a3db6861c68726b592b8b452c
erpesh <89254828+erpesh@users.noreply.github.com>
2022-06-23 14:07:35 +0300

    Add files via upload

---
+++ lab3/csahrp/csahrp/MultiSet.cs
@@ -0,0 +1,75 @@
+﻿namespace csahrp;
+
+public class MultiSet
+{
+    private char[] symbols { get; }
+
+    public void OutputSet()
+    {
+        var output = "";
+        foreach (var chr in symbols)
+        {
+            output += chr;
+        }
+        Console.WriteLine(output);
+        Console.WriteLine();
+    }
+
+    public MultiSet(char[] characters)
+    {
+        symbols = characters;
+    }
+
+    public MultiSet(int a, int b)
+    {
+        var characters = new char[b - a];
+        var index = 0;
+        for (var i = a; i < b; i++)
+        {
+            characters[index] = (char)i;
+            index++;
+        }
+
+        symbols = characters;
+    }
+
+    public MultiSet(int n)
+    {
+        var characters = new char[n];
+        var rand = new Random();
+        for (var i = 0; i < n; i++)
+        {
+            characters[i] = (char)rand.Next(32, 127);
+        }
+
+        symbols = characters;
+    }
+
+    public static MultiSet operator +(MultiSet a, MultiSet b)
+    {
+        var union = a.symbols.Union(b.symbols);
+        return new MultiSet(union.ToArray());
+    }
+
+    public static MultiSet operator *(MultiSet a, MultiSet b)
+    {
+        var intersect = a.symbols.Intersect(b.symbols);
+        return new MultiSet(intersect.ToArray());
+    }
+
+    public static MultiSet operator -(MultiSet a, MultiSet b)
+    {
+        var diff = a.symbols.Except(b.symbols);
+        return new MultiSet(diff.ToArray());
+    }
+
+    public bool Includes(char chr)
+    {
+        foreach (var symbol in symbols)
+        {
+            if (symbol == chr) return true;
+        }
+
+        return false;
+    }
+}
---
+++ lab3/csahrp/csahrp/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace csahrp
+{
+    class Program
+    {
+        public static MultiSet CalculateD(MultiSet A, MultiSet B, MultiSet C)
+        {
+            return (A + B) - C * B;
+        }
+
+        public static MultiSet CreateMultiset()
+        {
+            Console.WriteLine("Enter 1 if you want to create multiset manually and 0 if half-automatically, 2 if automatically");
+            var answer = Console.ReadLine();
+            
+            if (answer == "1")
+            {
+                return CreateManually();
+            }
+            
+            if (answer == "0")
+            {
+                return CreateAutomatically();
+            }
+
+            if (answer == "2")
+            {
+                return CreateFullAutomatically();
+            }
+            
+            return CreateMultiset();
+        }
+
+        public static MultiSet CreateManually()
+        {
+            int n;
+            Console.WriteLine("Enter size of multiset: ");
+            int.TryParse(Console.ReadLine(), out n);
+            if (n > 0)
+            {
+                var array = new char[n];
+                for (var i = 0; i < n; i++)
+                {
+                    array[i] = Console.ReadKey().KeyChar;
+                }
+
+                return new MultiSet(array);
+            }
+            return CreateManually();
+        }
+
+        public static MultiSet CreateAutomatically()
+        {
+            int a, b;
+            Console.WriteLine("Enter character range: ");
+            Console.WriteLine("a = ");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("b = ");
+            int.TryParse(Console.ReadLine(), out b);
+
+            if (a < b)
+            {
+                return new MultiSet(a, b);
+            }
+            return CreateAutomatically();
+        }
+
+        public static MultiSet CreateFullAutomatically()
+        {
+            int n;
+            Console.WriteLine("Enter set size:");
+            int.TryParse(Console.ReadLine(), out n);
+            return new MultiSet(n);
+        }
+
+        public static void CheckCharacters(MultiSet D)
+        {
+            char key = Console.ReadKey().KeyChar;
+            if (key == (char)7)
+            {
+                return;
+            }
+            Console.WriteLine(D.Includes(key) ? " - includes" : " - excludes");
+            CheckCharacters(D);
+        }
+        
+        static void Main(string[] args)
+        {
+            var A = CreateMultiset();
+            Console.WriteLine("\nA:");
+            A.OutputSet();
+            var B = CreateMultiset();
+            Console.WriteLine("\nB:");
+            B.OutputSet();
+            var C = CreateMultiset();
+            Console.WriteLine("\nC:");
+            C.OutputSet();
+            var D = CalculateD(A, B, C);
+            Console.WriteLine("D = (A + B) - C * B = ");
+            D.OutputSet();
+            Console.WriteLine("Enter character to see if it is in multiset D (enter CTRL + G to complete):");
+            CheckCharacters(D);
+        }
+        
+    }
+}
